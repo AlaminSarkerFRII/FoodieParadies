546b0d2078ac5d98c6c06da48a1366c4d4b03b72
alamin <alamin.education1997@gmail.com>
2022-11-16 12:53:18 +0600

    added navbar styles

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+
+const Navbar = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      {/* left side */}
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+
+        <h1 className="text-2xl sm:text-3xl lg:4xl px-2">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+
+        <div className=" hidden lg:flex justify-center items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black rounded-full text-white p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
+        </div>
+      </div>
+
+      {/* search input */}
+
+      <div className="rounded-full bg-gray-200 flex items-center w-[200px] sm:w-[400px] lg:w-[500px] px-2">
+        <AiOutlineSearch size={20} />
+
+        <input
+          className="bg-transparent focus:outline-none w-full p-2"
+          type="text"
+          placeholder="search food"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
