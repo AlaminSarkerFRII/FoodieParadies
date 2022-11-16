6c7242fd57a5352bf8d158d43b9d50684540b0bf
alamin <alamin.education1997@gmail.com>
2022-11-16 14:16:18 +0600

    added drawer menu & functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import { BsFillCartFill, BsTruck } from "react-icons/bs";
 import { FaUserFriends, FaWallet } from "react-icons/fa";
@@ -7,11 +7,13 @@ import { BiBarChartAlt2 } from "react-icons/bi";
 import { GiDinosaurBones } from "react-icons/gi";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* left side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
 
@@ -42,47 +44,52 @@ const Navbar = () => {
         <BsFillCartFill size={20} className="mr-2" /> Cart
       </button>
 
-      {/* mobile layout */}
+      {/* mobile layout Menu */}
 
-      <div className="bg-black/80 w-full h-screen z-10 top-0 left-0 fixed">
-        {/* overlay */}
+      {/* overlay */}
 
-        {/* side drawer menu*/}
-        <div className="fixed top-0 left-0 bg-white w-[300px] h-screen z-10 duration-300">
-          <h1 className="text-2xl p-4">
-            Best <span className="font-bold">Eats</span>
-          </h1>
-          <AiOutlineClose
-            size={20}
-            className="absolute cursor-pointer right-4 top-4"
-          />
+      {nav ? (
+        <div className="bg-black/80 w-full h-screen z-10 top-0 left-0 fixed"></div>
+      ) : (
+        ""
+      )}
 
-          <nav>
-            <ul className="flex flex-col p-4 text-gray-800">
-              <li className="flex items-center py-4 text-xl ">
-                <BsTruck size={25} className="mr-4" /> Orders
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <FaWallet size={25} className="mr-4" /> Wallet
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <MdHelp size={25} className="mr-4" /> Help
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <BiBarChartAlt2 size={25} className="mr-4" /> Promotions
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <MdFavorite size={25} className="mr-4" /> Favorite
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <GiDinosaurBones size={25} className="mr-4" /> Best Ones
-              </li>
-              <li className="flex items-center py-4 text-xl ">
-                <FaUserFriends size={25} className="mr-4" /> InviteFriends
-              </li>
-            </ul>
-          </nav>
-        </div>
+      {/* side drawer menu*/}
+      <div className={ nav ? 'fixed top-0 left-0 bg-white w-[300px] h-screen z-10 duration-300' : 'fixed top-0 left-[-100%] bg-white w-[300px] h-screen z-10 duration-300'}>
+        <h1 className="text-2xl p-4">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <AiOutlineClose
+        onClick={()=>setNav(!nav)}
+          size={20}
+          className="absolute cursor-pointer right-4 top-4"
+        />
+
+        <nav>
+          <ul className="flex flex-col p-4 text-gray-800 cursor-pointer">
+            <li className="flex items-center py-4 text-xl ">
+              <BsTruck size={25} className="mr-4" /> Orders
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <FaWallet size={25} className="mr-4" /> Wallet
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <MdHelp size={25} className="mr-4" /> Help
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <BiBarChartAlt2 size={25} className="mr-4" /> Promotions
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <MdFavorite size={25} className="mr-4" /> Favorite
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <GiDinosaurBones size={25} className="mr-4" /> Best Ones
+            </li>
+            <li className="flex items-center py-4 text-xl ">
+              <FaUserFriends size={25} className="mr-4" /> InviteFriends
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
